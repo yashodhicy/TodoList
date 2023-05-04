c8f7b6be69fb1a98bede4f1172f1fd9371709107
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-05-04 14:20:48 +0300

    add todolist basic styles and dyanamic part

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,16 +1,50 @@
 import _ from 'lodash';
 import './style.css';
+const todoListContainer = document.querySelector('.todolistbody');
 
+const todosData = [
+  {
+    index: 1,
+    description: 'Go shopping and buy new things',
+    completed: true,
+  },
+  {
+    index: 2,
+    description: 'Read a book',
+    completed: false,
+  },
+  {
+    index: 3,
+    description: 'Start a new job',
+    completed: true,
+  },
+  {
+    index: 4,
+    description: 'Read news quickly',
+    completed: false,
+  },
+];
 
-function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
- 
+const loadTodos = () => {
+  if (todosData.length > 0) {
+    todosData.forEach((todo) => {
+      const todoListItem = `<li class="todoListli"data-id="${todo.index}" data-completed="${todo.completed}">
+        <label for="${todo.index}">
+          <input type="checkbox" id="${todo.index}" value="${todo.id}"
+          ${todo.completed === true ? 'checked' : ''} />
+          <input class="listinput" type="text" value="${todo.description}" readonly />
+        </label>
+        <div class="action">
+        </div>
+      </li>`;
+      todoListContainer.querySelector('.todolist').innerHTML += todoListItem;
+      
+    });
+    const clear = document.createElement('li');
+    clear.classList.add('clearCompleted');
+    clear.innerText = 'Clear All Completed';
+    todoListContainer.querySelector('.todolist').appendChild(clear);
+classList.add('clearAll')  }
+};
 
-    // Add the image to our existing div.
-    return element;
-  }
-  
-  document.body.appendChild(component());
-  index.html
+loadTodos();
